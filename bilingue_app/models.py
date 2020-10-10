74ba90986cf417de1a40db9dec7d107355ae2831
23carnies <46233990+23carnies@users.noreply.github.com>
2020-10-10 14:17:03 -0400

    Add vocabulary model

--- bilingue_app/models.py
+++ bilingue_app/models.py
@@ -7,3 +7,10 @@ class User(AbstractUser):
     avatar = models.CharField(max_length=200)
     bio = models.TextField(max_length=500)
 
+class Vocabulary(models.Model):
+    english = models.CharField(max_length=100)
+    spanish = models.CharField(max_length=100)
+    cognates = models.CharField(max_length=200)
+    antonyms = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
