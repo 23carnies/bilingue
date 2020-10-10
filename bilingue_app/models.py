48d6287f6b1943ff3ffaab4b4eb9601b526a9b5f
23carnies <46233990+23carnies@users.noreply.github.com>
2020-10-10 14:51:47 -0400

    Adjust user and vocabulary  models

--- bilingue_app/models.py
+++ bilingue_app/models.py
@@ -6,11 +6,24 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     avatar = models.CharField(max_length=200)
     bio = models.TextField(max_length=500)
+    language = models.TextField(max_length=100)
 
 class Vocabulary(models.Model):
-    english = models.CharField(max_length=100)
-    spanish = models.CharField(max_length=100)
-    cognates = models.CharField(max_length=200)
-    antonyms = models.CharField(max_length=200)
+    english = models.CharField(
+        max_length=100,
+        blank=True
+        )
+    spanish = models.CharField(
+        max_length=100,
+        blank=True
+        )
+    cognates = models.CharField(
+        max_length=200,
+        blank=True
+        )
+    antonyms = models.CharField(
+        max_length=200,
+        blank=True
+        )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
