f680057e8d132a7cadc47515c208c3a60d91de7e
23carnies <46233990+23carnies@users.noreply.github.com>
2020-10-16 14:21:23 -0400

    Add new tab to spotify link

--- bilingue_app/views.py
+++ bilingue_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect 
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import login, get_user_model
 from django.contrib.auth.decorators import login_required
